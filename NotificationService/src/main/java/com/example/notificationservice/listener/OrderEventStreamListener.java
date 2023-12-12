0882d98837f2329d2f863dd78b3f180f59d4f67c
Inseo Kim <itsinseo@gmail.com>
2023-12-13 07:54:05 +0900

    feat: implement notification service

---
+++ NotificationService/src/main/java/com/example/notificationservice/listener/OrderEventStreamListener.java
@@ -0,0 +1,26 @@
+package com.example.notificationservice.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class OrderEventStreamListener implements StreamListener<String, MapRecord<String, String, String>> {
+
+    @Override
+    public void onMessage(MapRecord<String, String, String> message) {
+        Map<String, String> map = message.getValue();
+
+        String userId = map.get("userId");
+        String productId = map.get("productId");
+
+        // 주문 건에 대한 메일 발송 처리
+        log.info("[Order consumed] userId: " + userId + ", productId: " + productId);
+    }
+}
